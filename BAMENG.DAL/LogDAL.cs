e0a2d16aef8be9e70832c8ddbff9dff472e657d3
admin <admin@ADMIN-PC>
2016-11-25 11:14:34 +0800

    优惠券

--- BAMENG.DAL/LogDAL.cs
+++ BAMENG.DAL/LogDAL.cs
@@ -277,7 +277,61 @@ namespace BAMENG.DAL
                 return DbHelperSQLP.GetEntityList<StatisticsMoneyListModel>(dr);
             }
         }
-        
 
+        public List<StatisticsMoneyListModel> CouponStatisticsPieByBelongShop(int belongShopId, string startTime, string endTime)
+        {
+            string strSql = @"select log.ShopId,shop.ShopName as xData ,sum(log.Money) as yData  from BM_GetCashCouponLog as log
+                        left join BM_ShopManage as shop on log.shopId=shop.shopid
+                                where IsUse=1 and CONVERT(nvarchar(10), log.UseTime, 121)>=@startTime
+                                and CONVERT(nvarchar(10), log.UseTime, 121)<=@endTime and log.BelongOneShopId=@ShopId 
+                                group by log.ShopId,shop.ShopName
+                                order by sum(log.Money) desc";
+            var param = new[] {
+                new SqlParameter("@startTime",startTime),
+                new SqlParameter("@endTime",endTime),
+                new SqlParameter("@ShopId",belongShopId)
+            };
+            using (SqlDataReader dr = DbHelperSQLP.ExecuteReader(WebConfig.getConnectionString(), CommandType.Text, strSql, param))
+            {
+                return DbHelperSQLP.GetEntityList<StatisticsMoneyListModel>(dr);
+            }
+        }
+
+        public List<StatisticsMoneyListModel> CouponStatisticsPieByAdmin(string startTime, string endTime)
+        {
+            string strSql = @"select log.BelongOneShopId,shop.ShopName as xData ,sum(log.Money) as yData  from BM_GetCashCouponLog as log
+                        left join BM_ShopManage as shop on log.BelongOneShopId=shop.shopid
+                                where  IsUse=1 and CONVERT(nvarchar(10), log.UseTime, 121)>=@startTime
+                                and CONVERT(nvarchar(10), log.UseTime, 121)<=@endTime 
+                                group by log.BelongOneShopId,shop.ShopName
+                                order by sum(log.Money) desc";
+            var param = new[] {
+                new SqlParameter("@startTime",startTime),
+                new SqlParameter("@endTime",endTime)
+            };
+            using (SqlDataReader dr = DbHelperSQLP.ExecuteReader(WebConfig.getConnectionString(), CommandType.Text, strSql, param))
+            {
+                return DbHelperSQLP.GetEntityList<StatisticsMoneyListModel>(dr);
+            }
+        }
+
+        public List<StatisticsMoneyListModel> CouponStatisticsPieByShop(int shopId, string startTime, string endTime)
+        {
+            string strSql = @"select log.BelongOneUserId ,shop.UB_UserRealName as xData ,sum(log.Money) as yData  from BM_GetCashCouponLog as log
+                        left join Hot_UserBaseInfo as shop on log.BelongOneUserId=shop.UB_UserID
+                                where IsUse=1 and CONVERT(nvarchar(10), log.UseTime, 121)>=@startTime
+                                and CONVERT(nvarchar(10), log.UseTime, 121)<=@endTime and log.ShopId=@ShopId 
+                                group by log.BelongOneUserId,shop.UB_UserRealName
+                                order by sum(log.Money) desc";
+            var param = new[] {
+                new SqlParameter("@startTime",startTime),
+                new SqlParameter("@endTime",endTime),
+                new SqlParameter("@ShopId",shopId)
+            };
+            using (SqlDataReader dr = DbHelperSQLP.ExecuteReader(WebConfig.getConnectionString(), CommandType.Text, strSql, param))
+            {
+                return DbHelperSQLP.GetEntityList<StatisticsMoneyListModel>(dr);
+            }
+        }
     }
 }
